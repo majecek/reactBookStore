ed0f96cac48724af8d296029d9c71b3f9cf19519
Marek Chowaniok <marek.chowaniok@gmail.com>
2016-12-29 17:57:19 -0600

    implementing redux

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by marek on 29/12/2016.
+ */
+import React, { Component } from 'react';
+import BookList from './../containers/booklist';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <BookList/>
+            </div>
+        );
+    }
+}
+
+export default App;
