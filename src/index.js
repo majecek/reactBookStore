b0ecf7e594e8ad7816a2fc3188b7573eb276ada2
Marek Chowaniok <marek.chowaniok@gmail.com>
2016-12-30 16:07:04 -0600

    adding files to projects

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
+import './index.css';
+
+import App from './components/app';
+import reducers from './reducers'
+
+const createStoreWithMiddleWare = applyMiddleware()(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleWare(reducers)}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
